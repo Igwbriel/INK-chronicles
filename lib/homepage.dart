cada5424b674833fc558b69c3c96a719b1250025
Gabriel Dias <GabrielDias7820@gmail.com>
2023-06-01 11:23:19 -0300

    add background image na homepage

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -7,29 +7,40 @@ class Homepage extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      theme: ThemeData(primarySwatch: Colors.deepOrange),
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Color.fromARGB(255, 46, 46, 46),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: <Widget>[
-              Image.asset('assets/images/inkLogo.png'),
-              SizedBox(
-                width: size.width * 0.8,
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, 'Initial');
-                  },
-                  child: Text('Entrar', style: TextStyle(fontSize: 50)),
-                  style: ElevatedButton.styleFrom(
-                    padding: EdgeInsets.symmetric(vertical: 20, horizontal: 40),
-                  ),
-                ),
+        body: Stack(
+          children: <Widget>[
+            Positioned.fill(
+              child: Image.network(
+                'https://download.memgraph.com/asset/playground/marvel-comics-background-fde3e300.jpg',
+                fit: BoxFit.cover,
               ),
-            ],
-          ),
+            ),
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Image.asset('assets/images/inkLogo.png'),
+                  SizedBox(
+                    width: size.width * 0.8,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, 'Initial');
+                      },
+                      child: Text('Entrar', style: TextStyle(fontSize: 50)),
+                      style: ElevatedButton.styleFrom(
+                        padding:
+                            EdgeInsets.symmetric(vertical: 20, horizontal: 40),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
