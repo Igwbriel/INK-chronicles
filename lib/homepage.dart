8ae32c276c04fcb0220a0d639be23de0bf87462c
Gedsonfa <106558706+Gedsonfa@users.noreply.github.com>
2023-06-01 21:14:21 -0300

    botão preto

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -2,12 +2,27 @@ import 'package:flutter/material.dart';
 
 class Homepage extends StatelessWidget {
   const Homepage({Key key = const ValueKey('homepage')}) : super(key: key);
+  final MaterialColor black = const MaterialColor(
+    0xFF000000,
+    <int, Color>{
+      50: Color(0xFFE5E5E5),
+      100: Color(0xFFBFBFBF),
+      200: Color(0xFF999999),
+      300: Color(0xFF737373),
+      400: Color(0xFF4D4D4D),
+      500: Color(0xFF262626),
+      600: Color(0xFF1F1F1F),
+      700: Color(0xFF191919),
+      800: Color(0xFF121212),
+      900: Color(0xFF0C0C0C),
+    },
+  );
 
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.deepOrange),
+      theme: ThemeData(primarySwatch: black),
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Color.fromARGB(255, 46, 46, 46),
