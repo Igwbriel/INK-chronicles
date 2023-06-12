aeae78caaa9f8d1a9e19e5bcf245622c72934a3c
Gedsonfa <106558706+Gedsonfa@users.noreply.github.com>
2023-06-12 00:41:09 -0300

    pesquisa pronta

--- lib/routes.dart
+++ lib/routes.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     
-    return MaterialApp(initialRoute: '/'
+    return MaterialApp(initialRoute: 'Search'
     , routes: {
       '/': (context) => Homepage(),
       'Initial': (context) => Apis(),
